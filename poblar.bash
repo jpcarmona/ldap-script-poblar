67941119fc1ac186ca326275ee88df615d7357e3
jpcarmona <jpcarmona92@gmail.com>
2018-11-18 00:04:15 +0100

    Inserción de comentarios final para entrega, corrección error de variable en uidNumber gidNumber y cambio de orden en los objectClass

--- poblar.bash
+++ poblar.bash
@@ -5,7 +5,6 @@
 #Descripción: Poblar directorio LDAP con usuarios
 
 # Comprobamos que se especifica fichero en parámetro $1
-
 if [ -z "$1" ]
 then
 	echo "Para ejecutar este script debes especificar un fichero de entrada CSV"
@@ -13,16 +12,16 @@ then
 	exit
 fi
 
-# Comprobamos que se especifica contraseña en parámetro $2
 
+# Comprobamos que se especifica contraseña en parámetro $2
 if [ -z "$2" ]
 then
 	echo "Por favor escriba la contraseña -admin- del directorio"
 	exit
 fi
 
-# Comprobamos que se especifica contraseña en parámetro $2
 
+# Comprobamos que la contraseña es correcta
 error=$(ldapsearch -x -D "cn=admin,dc=juanpe,dc=gonzalonazareno,dc=org" \
 -b "dc=juanpe,dc=gonzalonazareno,dc=org" -w "$2" "cn=admin" 2>& 1>/dev/null )
 
@@ -32,13 +31,24 @@ then
 	exit
 fi
 
+
 # Creamos variable uidnum "uidNumber"
 uidnum=2000
 
+
 # Empezamos bucle para leer fichero CSV desde parámetro $1
+## En IFS especificamos el delimitador de un valor en cada línea.
+## Luego se asigna cada valor a cada variable "nombre apellidos email usuario pubkey".
+## En este caso sabemos que solo hay 5 valores en cada línea, pero en caso de que hubiera más
+## se le asignaría el valor desde el 5º delimitador hasta el final de la línea a la variable 
+## "pubkey" en este caso.
+##-------------------------------- Inicio Bucle while ------------------------------------
 while IFS=: read nombre apellidos email usuario pubkey
 do
 
+# Inserción de usuarios con ldapsearch
+## Además redirigimos la salida del error estándar a la variable "error2" para luego realizar 
+## un aviso de los errores
 error2=$(ldapadd -x -D 'cn=admin,dc=juanpe,dc=gonzalonazareno,dc=org' -w "$2" << EOF 2>& 1>/dev/null
 dn: uid=$usuario,ou=People,dc=juanpe,dc=gonzalonazareno,dc=org
 objectClass: top
@@ -46,16 +56,18 @@ objectClass: posixAccount
 objectClass: inetOrgPerson
 objectClass: ldapPublicKey
 uid: $usuario
-uidNumber: 2000
-gidNumber: $uidnum
+uidNumber: $uidnum
+gidNumber: 2000
 homeDirectory: /home/$usuario
 cn: $nombre $apellidos
 givenName: $nombre
 sn: $apellidos
 mail: $email
+sshPublicKey: $pubkey
 EOF
 )
 
+# Aviso de errores
 if [ -n "$error2" ]
 then
 echo "Error al insertar el usuario $usuario"
@@ -63,9 +75,8 @@ echo "ERROR: -$error2-"
 echo ""
 fi
 
-
-
+# Sumamos +1 a "uidnum" en cada vuelta
 uidnum=$((uidnum + 1))
 
 done < $1
-
+##-------------------------------- Final Bucle while -------------------------------------
