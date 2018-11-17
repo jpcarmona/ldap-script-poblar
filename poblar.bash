bc687460e2ecba9205137667d7f1e2b60259ff65
jpcarmona <jpcarmona92@gmail.com>
2018-11-17 19:00:25 +0100

    Creación de bucle para leer fichero den entrada en

---
+++ poblar.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+#Nombre del archivo: poblar.bash
+#Fecha de creación: 17/11/2018
+#Autor: Juan pedro Carmona Romero
+#Descripción: Poblar directorio LDAP con usuarios
+
+# Empezamos bucle para leer fichero CSV
+
+while IFS=: read nombre apellidos email usuario pubkey
+do
+	echo "$nombre -- $apellidos -- $email -- $usuario -- $pubkey
+done < $1
