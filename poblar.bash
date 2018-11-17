af26a43c34bb1a1a64bc5e70e79ce379d93719e5
jpcarmona <jpcarmona92@gmail.com>
2018-11-17 22:48:00 +0100

    Arreglo comprobación de contraseña correcta

--- poblar.bash
+++ poblar.bash
@@ -6,7 +6,7 @@
 
 # Comprobamos que se especifica fichero en parámetro $1
 
-if [ -z "$1"]
+if [ -z "$1" ]
 then
 	echo "Para ejecutar este script debes especificar un fichero de entrada CSV"
 	echo "Ejemplo: bash poblar.bash usuarios.csv"
@@ -15,7 +15,7 @@ fi
 
 # Comprobamos que se especifica contraseña en parámetro $2
 
-if [ -z "$2"]
+if [ -z "$2" ]
 then
 	echo "Por favor escriba la contraseña -admin- del directorio"
 	exit
@@ -23,17 +23,16 @@ fi
 
 # Comprobamos que se especifica contraseña en parámetro $2
 
-error=$(ldapsearch -x -D "cn=admin,dc=juanpe,dc=gonzalonazareno,dc=org"/
--b "dc=juanpe,dc=gonzalonazareno,dc=org" -w rot "cn=admin" 1>/dev/null &>1 )
+error=$(ldapsearch -x -D "cn=admin,dc=juanpe,dc=gonzalonazareno,dc=org" \
+-b "dc=juanpe,dc=gonzalonazareno,dc=org" -w "$2" "cn=admin" 2>& 1>/dev/null )
 
-if [ -n "$error"]
+if [ -n "$error" ]
 then
 	echo "Contraseña Incorrecta"
 	exit
 fi
 
-# Empezamos bucle para leer fichero CSV desde parámetro $1
-
+ Empezamos bucle para leer fichero CSV desde parámetro $1
 while IFS=: read nombre apellidos email usuario pubkey
 do
 
